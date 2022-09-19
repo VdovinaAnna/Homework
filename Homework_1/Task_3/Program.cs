5bee2dbd280eab1e91afb450bda557481da29a12
Anna <vdovin-anna@yandex.ru>
2022-09-19 22:37:27 +0700

    Решена задача 6

---
+++ Homework_1/Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int z = a % 2;
+if (z == 0)
+{
+    Console.WriteLine ("Число четное");
+}
+else
+{
+    Console.WriteLine ("Число нечетное");
+}
