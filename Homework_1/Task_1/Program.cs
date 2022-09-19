f225e9810dbd89d4213bf44162aa399808d2710e
Anna <vdovin-anna@yandex.ru>
2022-09-19 21:33:45 +0700

    Решена задача 2

---
+++ Homework_1/Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int min = a, max = b;
+if(a==b)
+{
+ Console.WriteLine ("Числа равны");
+}
+else
+{
+    if (a < b)
+    {
+    min = a; 
+    max = b;
+    Console.WriteLine ("min = " + min);
+    Console.WriteLine ("max = " + max);
+    }
+    else
+    {
+    min = b;
+    max = a;
+    Console.WriteLine ("min = " + min);
+    Console.WriteLine ("max = " + max);
+    }
+}
