fd18b4067bcfb8086a0b9473e9b1a0c79f35b90f
Anna <vdovin-anna@yandex.ru>
2022-09-19 21:55:07 +0700

    Решена задача 4

---
+++ Homework_1/Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(max > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if(max < c )
+{
+   max = c; 
+}
+Console.WriteLine ("Максимальное число: "+ max);
