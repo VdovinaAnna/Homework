a2b5ff2ff09548b25f8fde57fc6c52b343622e4c
Anna <vdovin-anna@yandex.ru>
2022-09-19 23:09:50 +0700

    Решена задача 8

---
+++ Homework_1/Task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+int i = m;
+while(i <= N)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+            i = i+1;
+        }
