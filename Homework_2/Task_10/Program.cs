1a78c6aae8cdbd618e1e745c07732ec0de33edcc
Anna <vdovinanna1991@gmail.com>
2022-09-26 13:58:09 +0300

    Решена задача 10

---
+++ Homework_2/Task_10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Project
+{
+    class Programm
+    {
+    static void Main(string[] args)
+    {
+        void Zadacha10()
+        {
+        Console.WriteLine("Введите трехзначное число");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 99 && number < 1000)
+        {
+            Console.WriteLine("Вторая цифра числа " + (number/10%10));
+        }
+        else
+        {
+            Console.WriteLine("Введено не трехзначное число");
+        }
+        }
+        Zadacha10();
+    }
+    #region Fill & Print
+    }
+}
+#endregion
