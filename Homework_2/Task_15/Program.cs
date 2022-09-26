73a1177ad35965a1688d6a4bec1692e868ad7b51
Anna <vdovinanna1991@gmail.com>
2022-09-26 17:55:31 +0300

    Решена задача 15

---
+++ Homework_2/Task_15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Project
+{
+    class Programm
+    {
+    static void Main(string[] args)
+    {
+        void Zadacha15()
+        /* Задача 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+        и проверяет, является ли этот день выходным.*/
+        {
+        int day = 7;
+
+        if (day >0 && day < 8)
+        {
+            if (day == 6 || day == 7)
+            {
+            Console.WriteLine("Выходной день");
+            }
+            else
+            {
+            Console.WriteLine("Будний день");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неверный ввод");
+        }
+        }
+        Zadacha15();
+    }
+    #region Fill & Print
+    }
+}
+#endregion
