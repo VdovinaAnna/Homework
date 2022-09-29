2f3b5864816ade1aeba979058004e76fae385aa2
Anna <vdovinanna1991@gmail.com>
2022-09-29 11:22:16 +0300

    Решена задача 23

---
+++ Homework_3/Task_23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Project
+{
+    class Programm
+    {
+    static void Main(string[] args)
+    {
+        void Zadacha23()
+        /* Задача 23. Напишите программу, которая принимает на вход число (N) 
+        и выдаёт таблицу кубов чисел от 1 до N.*/
+        {
+        Console.WriteLine("Введите число");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i<= number; i++)
+        {
+            Console.WriteLine($"{i} * {i} * {i} = {Math.Pow(i,3)}");
+        }
+        
+        }
+        Zadacha23();
+    }
+    #region Fill & Print
+    }
+}
+#endregion
