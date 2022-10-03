13b4cb99aad0cf53e2edcf15a6f6b76cb5caa960
Anna <vdovinanna1991@gmail.com>
2022-10-03 18:54:43 +0300

    Решена задача 27

---
+++ Homework_4/Task_27/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Project
+{
+    class Programm
+    {
+    static void Main(string[] args)
+    {
+        void Zadacha27()
+        /* Напишите программу, которая принимает на вход число 
+        и выдаёт сумму цифр в числе.*/
+        {
+        Console.WriteLine("Введите число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+        while (a > 0)
+        {
+            sum += a%10;
+            a /= 10;
+        }
+        Console.WriteLine($"Сумма цифр числа равна {sum}");
+        }
+    Zadacha27();
+    #region Fill & Print
+    }
+}
+}
+#endregion
