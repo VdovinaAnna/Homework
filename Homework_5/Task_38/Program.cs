9ebf1aabbde824d63223e5b392c300e7dfb3e0af
Anna <vdovinanna1991@gmail.com>
2022-10-06 15:23:02 +0300

    Решена задача 38

---
+++ Homework_5/Task_38/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Project
+{
+    class Programm
+    {
+    static void Main(string[] args)
+    {
+        void Zadacha38()
+        /* Задача 38: Задайте массив вещественных чисел. 
+        Найдите разницу между максимальным и минимальным элементов массива.*/
+        {
+        int [] array = new int [5];
+        int sum = 0;
+        
+        for (int i = 0; i < array.Length; i ++)
+        {
+            array[i] = new Random().Next (100,1000);
+            Console.WriteLine(array[i] + " " );
+            if (i % 2 == 1) sum +=array[i];
+        }
+        Console.WriteLine ("Сумма элементов, стоящих на нечетных позициях: " + sum);
+        }
+    Zadacha38();
+    #region Fill & Print
+    }
+}
+}
+#endregion
+
