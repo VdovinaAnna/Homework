e18517b78d438a0dcbf1c4c75ff2b3d0720d2d9c
Anna <vdovinanna1991@gmail.com>
2022-10-06 15:05:04 +0300

    Решена задача 34

---
+++ Homework_5/Task_34/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Project
+{
+    class Programm
+    {
+    static void Main(string[] args)
+    {
+        void Zadacha34()
+        /* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+        Напишите программу, которая покажет количество чётных чисел в массиве.*/
+        {
+        int [] array = new int [8];
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next (10,100);
+            Console.Write(array[i] + " ");
+            if (array[i] % 2 == 0) count++;
+        }
+        Console.WriteLine(" Количество четных элементов: " +count);
+        }
+    Zadacha34();
+    #region Fill & Print
+    }
+}
+}
+#endregion
