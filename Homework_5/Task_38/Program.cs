0f244754c793538a81c78fbbacce4b2756023b9f
Anna <vdovinanna1991@gmail.com>
2022-10-06 18:56:17 +0300

    Pешена задача 38

--- Homework_5/Task_38/Program.cs
+++ Homework_5/Task_38/Program.cs
@@ -5,23 +5,69 @@ namespace Project
     class Programm
     {
     static void Main(string[] args)
-    {
-        void Zadacha38()
-        /* Задача 38: Задайте массив вещественных чисел. 
+{ /* Задача 38: Задайте массив вещественных чисел. 
         Найдите разницу между максимальным и минимальным элементов массива.*/
+
+void FillArray(double[] array)
+{
+    Random random = new Random();
+    for(int i = 0; i < array.Length; i++)
         {
-        int [] array = new int [5];
-        int sum = 0;
-        
-        for (int i = 0; i < array.Length; i ++)
+            array[i] = Math.Round (random.NextDouble() * 20 - 10, 2);
+        } 
+}
+
+void PrintArray(double[] arr)
+    {
+    int size = arr.Length;
+    Console.WriteLine("Вывод массива: ");
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}    
+
+double MaxElement (double[] array)
+{
+    double maxElement = array[0]; 
+    for (int i = 1; i < array.Length; i++) 
+    {
+        if (array[i] > maxElement) 
         {
-            array[i] = new Random().Next (100,1000);
-            Console.WriteLine(array[i] + " " );
-            if (i % 2 == 1) sum +=array[i];
+            maxElement = array[i]; 
         }
-        Console.WriteLine ("Сумма элементов, стоящих на нечетных позициях: " + sum);
+    }
+    return maxElement;
+}
+
+double MinElement (double[] array)
+{
+    double minElement = array[0]; 
+    for (int i = 1; i < array.Length; i++) 
+    {
+        if (array[i] < minElement) 
+        {
+            minElement = array[i]; 
         }
-    Zadacha38();
+    }
+    return minElement;
+}
+
+double GetDiffMaxMin (double[] array, double maxElement, double minElement) 
+{
+    double diff = Math.Round((maxElement - minElement), 2); 
+    return diff;
+}
+
+double[] arr = new double[8];
+FillArray(arr);
+PrintArray(arr);
+double maxElement = MaxElement(arr);
+Console.WriteLine("Максимальный элемент: " + maxElement + ".");
+double minElement = MinElement(arr);
+Console.WriteLine("Минимальный элемент: " + minElement + ".");
+Console.WriteLine("Разница между числами: " + GetDiffMaxMin(arr, maxElement, minElement) + ".");
     #region Fill & Print
     }
 }
