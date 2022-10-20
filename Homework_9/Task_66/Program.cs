dea446ee8e3c87317a56a01e621f2f2b25d6d15e
Anna <vdovinanna1991@gmail.com>
2022-10-20 12:16:29 +0300

    Решена задача 66

---
+++ Homework_9/Task_66/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Project
+{
+    class Programm
+    {
+    static void Main(string[] args)
+    { /*Задача 66: Задайте значения M и N. Напишите программу, 
+    которая найдёт сумму натуральных элементов в промежутке от M до N.*/
+
+double SumFor(double firstNumber, double lastNumber)
+{
+    double result = 0;
+    if (firstNumber < lastNumber)
+    {
+        result = firstNumber + SumFor(firstNumber + 1, lastNumber);
+    }
+    else if (firstNumber == lastNumber)
+    {
+         result = result + lastNumber;
+    }
+    return result;
+}
+
+double m = 2;
+double n = 6;
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} = {SumFor(m, n)}");
+
+ }
+}
+}
+
