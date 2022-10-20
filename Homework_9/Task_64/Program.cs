46821b2b61fcb85e31322b36aabd9641469f5acd
Anna <vdovinanna1991@gmail.com>
2022-10-20 12:06:19 +0300

    Решена задача 64

---
+++ Homework_9/Task_64/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Project
+{
+    class Programm
+    {
+    static void Main(string[] args)
+    { /*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+    в промежутке от N до 1. Выполнить с помощью рекурсии.*/
+
+    int number = 10;
+    Zadacha64(number);
+    void Zadacha64 (int number, int count = 0)
+    {
+        if (number > 1)
+    {
+        Console.Write($"{number}, ");
+        Zadacha64(number - 1);
+    }
+    else if (number == 1)
+    {
+        Console.Write(number);
+        Console.WriteLine();
+    }
+    }
+ }
+}
+}
+
